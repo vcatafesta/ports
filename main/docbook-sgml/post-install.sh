e325a3d74f6421238a11ac6968792623c593ce56
vcatafesta <vcatafesta@gmail.com>
2023-09-09 04:56:29 -0400

    sáb 09 set 2023 04:56:28 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ main/docbook-sgml/post-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat >> /usr/share/sgml/docbook/sgml-dtd-4.5/catalog << "EOF"
+  -- Begin Single Major Version catalog changes --
+
+PUBLIC "-//OASIS//DTD DocBook V4.4//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.3//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.2//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.1//EN" "docbook.dtd"
+PUBLIC "-//OASIS//DTD DocBook V4.0//EN" "docbook.dtd"
+
+  -- End Single Major Version catalog changes --
+EOF
