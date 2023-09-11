569c35a66da0608793e123a0b0907f0413401e2d
vcatafesta <vcatafesta@gmail.com>
2023-09-11 07:58:47 -0400

    seg 11 set 2023 07:58:47 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- testing/docbook-sgml/post-install.sh
+++ testing/docbook-sgml/post-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat /usr/share/sgml/docbook/sgml-dtd-4.5/catalog
+install-catalog --add /etc/sgml/sgml-docbook-dtd-4.5.cat /etc/sgml/sgml-docbook.cat
+
 cat >> /usr/share/sgml/docbook/sgml-dtd-4.5/catalog << "EOF"
   -- Begin Single Major Version catalog changes --
 
