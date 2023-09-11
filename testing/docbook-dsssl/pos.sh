8ea3a08c26201e7a245691fab6b5f02dcd2ba838
vcatafesta <vcatafesta@gmail.com>
2023-09-11 09:02:49 -0400

    seg 11 set 2023 09:02:49 -04 Vilmar Catafesta (vcatafesta@gmail.com)

---
+++ testing/docbook-dsssl/pos.sh
@@ -0,0 +1,3 @@
+install-catalog --add /etc/sgml/dsssl-docbook-stylesheets.cat /usr/share/sgml/docbook/dsssl-stylesheets-1.79/catalog  > /dev/null 2>&1
+install-catalog --add /etc/sgml/dsssl-docbook-stylesheets.cat /usr/share/sgml/docbook/dsssl-stylesheets-1.79/common/catalog  > /dev/null 2>&1
+install-catalog --add /etc/sgml/sgml-docbook.cat /etc/sgml/dsssl-docbook-stylesheets.cat  > /dev/null 2>&1
